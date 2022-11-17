1e8ba08a2a80b91679e5d3a7522e94aa044dbda6
Joel Parra <joelramonph@gmail.com>
2022-11-17 18:28:32 -0400

    add errorpage

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,7 +4,12 @@ const Error = () => {
   return (
     <Layout>
   
-   <h2>Error</h2>
+  <main className='error-page'>
+    <section>
+      <h1>404</h1>
+      <h3>Page no found</h3>
+    </section>
+  </main>
   
   </Layout>
   )
