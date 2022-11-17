8382380ec1007a501db4b8a9fec1dfec9b56c708
Joel Parra <joelramonph@gmail.com>
2022-11-17 09:24:27 -0400

    initial config

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import * as React from "react"
+import Layout from "../componets/Layout"
+
+export default function Home() {
+return (
+  <Layout>
+  
+   <h2>Hola Mundo</h2>
+  
+  </Layout>
+)
+}
---
+++ src/pages/recipes.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Layout from '../componets/Layout'
+const recipes = () => {
+  return (
+    <Layout>
+
+    <h2>recipes</h2>
+
+    </Layout>
+  )
+}
+
+export default recipes
