c5f100a7683f86c1b2a066b7b923ac418f7223bf
Joel Parra <joelramonph@gmail.com>
2022-11-20 10:17:15 -0400

    addabout page and image

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,13 +1,42 @@
-import React from 'react'
-import Layout from '../componets/Layout'
+import React from "react";
+import Layout from "../componets/Layout";
+import { StaticImage } from "gatsby-plugin-image";
+import { Link } from "gatsby";
+
 const About = () => {
   return (
     <Layout>
+      <main className="page">
+        <section className="about-page">
+          <article>
+            <h2>
+              I'm baby godard Brooklyn gastropub.
+              
+            </h2>
+            <p>
+              Fashion axe literally plaid authentic polaroid. Adaptogen
+              aesthetic edison bulb, wayfarers shaman celiac normcore mukbang
+              four dollar toast plaid tote bag.
+            </p>
+            <p>
+              Four dollar toast activated charcoal mukbang health goth tofu.
+              Readymade man bun flexitarian four dollar toast.
+            </p>
+            <Link to="/contact" className="btn">
+              contact
+            </Link>
+          </article>
 
-    <h2>I am About</h2>
-    
+          <StaticImage
+            src="../assets/images/about.jpeg"
+            alt="persont salt in bowl"
+            className="about-img"
+            placeholder="blurred"
+          />
+        </section>
+      </main>
     </Layout>
-  )
-}
+  );
+};
 
-export default About
+export default About;
